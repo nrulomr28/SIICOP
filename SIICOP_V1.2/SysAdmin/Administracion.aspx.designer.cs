9265f1c41f6d3ce1f6ec53a0d8fa598951e5d7b1
rmestizor <rmestizor@sspver.gob.mx>
2026-06-15 13:50:10 -0600

    feat(sysadmin): crear modulo centralizado de administracion

---
+++ SIICOP_V1.2/SysAdmin/Administracion.aspx.designer.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SIICOP_V1._2.sysadmin
+{
+
+
+    public partial class Administracion
+    {
+
+        /// <summary>
+        /// Control lblUsuariosActivos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUsuariosActivos;
+    }
+}
